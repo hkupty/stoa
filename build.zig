acef3836c7a7f6c390c59b5df9adcb057dbee6c4
Henry Kupty <hkupty@gmail.com>
2025-09-26 22:41:49 +0200

    refactor: Remove transient
    No need to be an extra binary, it can be a simple command in zsh

--- build.zig
+++ build.zig
@@ -9,20 +9,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
-    const transient = b.addExecutable(.{
-        .name = "stoa-transient",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/transient.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "stoa", .module = mod },
-            },
-        }),
-    });
-
-    b.installArtifact(transient);
-
     const session = b.addExecutable(.{
         .name = "stoa-session",
         .root_module = b.createModule(.{
@@ -71,13 +57,6 @@ pub fn build(b: *std.Build) void {
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
-    const exe_tests = b.addTest(.{
-        .root_module = transient.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
 }
