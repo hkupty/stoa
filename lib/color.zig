e0039046786929c6eb6e51379efc37563834a588
Henry Kupty <hkupty@gmail.com>
2025-10-03 09:33:23 +0200

    refactor: calculate ascii value instead of formatting
    This removes the escaping required by bufPrint

--- lib/color.zig
+++ lib/color.zig
@@ -2,10 +2,24 @@ const std = @import("std");
 
 const Writer = std.Io.Writer;
 
+var buf: [3]u8 = undefined;
+fn to_ascii(number: u8) usize {
+    var num = number;
+    var index: usize = 3;
+    while (num > 0) {
+        index -= 1;
+        const digit = num % 10;
+        buf[index] = digit + '0';
+        num /= 10;
+    }
+    return index;
+}
+
 pub fn write_color(writer: *Writer, color: u8) void {
-    var buf: [256]u8 = undefined;
-    const out = std.fmt.bufPrint(&buf, "%{{\x1B[38;5;{d}m%}}", .{color}) catch unreachable;
-    _ = writer.write(out) catch unreachable;
+    _ = writer.write("%{\x1B[38;5;") catch unreachable;
+    const sz = to_ascii(color);
+    _ = writer.write(buf[sz..]) catch unreachable;
+    _ = writer.write("m%}") catch unreachable;
 }
 
 pub fn clear_format(writer: *Writer) void {
